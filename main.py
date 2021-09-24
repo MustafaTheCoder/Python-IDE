201c8e47717d9ffc3d1cb0776c05da2ff47dae7d
MustafaTheCoder <73116655+MustafaTheCoder@users.noreply.github.com>
2021-09-24 21:46:31 +0500

    lol

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from tkinter.filedialog import asksaveasfilename, askopenfilename
 import subprocess
 
 compiler = Tk()
-compiler.title('My Fantastic IDE')
+compiler.title('Python IDE - MustafaXD')
 file_path = ''
 
 
